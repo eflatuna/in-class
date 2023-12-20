d8ff6b8fe8c73294a6bd88b22d7f660888dafbe5
esra <esrabaskaya84@gmail.com>
2023-12-20 11:34:23 +0100

    variables dosyasi acildi

---
+++ javascript/class-notes/js-01-Intro-Variables/app.js
@@ -0,0 +1,35 @@
+console.log("Konsol icindeyim")
+// console.log(object);
+console.log("Hatanin altindaki satirdayim");
+console.error("Bu bir hata mesajidir");
+console.warn("Bu bir uyari mesajidir");
+// console.clear()
+
+console.table(
+  [
+    {name:"Harvey",age:35},
+    {name:"Helen",age:45}
+
+  ]
+)
+
+
+
+// alert("Bu bir uyaridir")
+
+// yas=25
+// if (yas<30) {
+//   alert("Biraz daha büyü de gel")
+// }
+
+document.write (" <b> Browserda sonuc gösterme <br></b>")
+document.write("Bu ikinci satir")
+a=14
+b=45
+toplam=a+b
+document.write("Sonuc:",toplam,"esra")
+console.log("sonuc:",toplam)
+
+// ad=prompt("Adinizi giriniz")
+// console.log(ad)
+// document.write(ad)
